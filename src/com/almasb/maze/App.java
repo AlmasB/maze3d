c84cf64deb72f1150939c93a2bf14e1d4ef1207a
AlmasB <almaslvl@gmail.com>
2015-02-08 00:28:12 +0000

    added maze generator and basic layout for maze with render, added basic ambient light

--- src/com/almasb/maze/App.java
+++ src/com/almasb/maze/App.java
@@ -1,11 +1,91 @@
 package com.almasb.maze;
 
+import com.almasb.maze.MazeGenerator.MazeCell;
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+import com.jme3.util.TangentBinormalGenerator;
 
 public class App extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        initLight();
+        initMaze(10, 6);
+    }
 
+    private void initLight() {
+        AmbientLight generalLight = new AmbientLight();
+        generalLight.setColor(ColorRGBA.White.mult(1f));
+        rootNode.addLight(generalLight);
+    }
+
+    private void initMaze(int mazeSize, int wallSize) {
+        MazeCell[][] maze = new MazeGenerator(mazeSize, mazeSize).getMaze();
+
+        Node mazeNode = new Node("Maze");
+
+        // build wall geometry
+        Box wallMesh = new Box(wallSize, 6f, 0.5f);
+        Geometry wallGeo = new Geometry("Wall", wallMesh);
+        TangentBinormalGenerator.generate(wallMesh);           // to use normal maps
+
+        Material wallMat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        wallMat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/wall.jpg"));
+        wallMat.setTexture("NormalMap", assetManager.loadTexture("Textures/wall_normals.jpg"));
+        wallMat.setBoolean("UseMaterialColors", true);
+        wallMat.setColor("Diffuse", ColorRGBA.White);
+        wallMat.setColor("Specular", ColorRGBA.White);
+        wallMat.setColor("Ambient", ColorRGBA.White);
+        wallMat.setFloat("Shininess", 8f);  // [0,128]
+
+        wallGeo.setMaterial(wallMat);
+        wallGeo.setShadowMode(ShadowMode.CastAndReceive);
+
+
+        for (int i = 0; i < mazeSize; i++) {
+            for (int j = 0; j < mazeSize; j++) {
+                MazeCell cell = maze[j][i];
+                if (cell.leftWall) {
+                    Geometry wall = wallGeo.clone();
+                    wall.rotate(0, 90 * FastMath.DEG_TO_RAD, 0);
+                    wall.setLocalTranslation(j*wallSize*2 - wallSize, 6f,  i*wallSize*2 + wallSize);
+
+                    mazeNode.attachChild(wall);
+                }
+
+                if (cell.topWall) {
+                    Geometry wall = wallGeo.clone();
+                    wall.setLocalTranslation(j*wallSize*2, 6f, i*wallSize*2);
+
+                    mazeNode.attachChild(wall);
+                }
+
+                // right side
+                if (j == mazeSize - 1) {
+                    Geometry wall = wallGeo.clone();
+                    wall.rotate(0, 90 * FastMath.DEG_TO_RAD, 0);
+                    wall.setLocalTranslation(j*wallSize*2 + wallSize, 6f,  i*wallSize*2 + wallSize);
+
+                    mazeNode.attachChild(wall);
+                }
+            }
+        }
+
+        // bottom of the maze
+        for (int j = 0; j < mazeSize; j++) {
+            Geometry wall = wallGeo.clone();
+            wall.setLocalTranslation(j*wallSize*2, 6f, mazeSize*wallSize*2);
+
+            mazeNode.attachChild(wall);
+        }
+
+        rootNode.attachChild(mazeNode);
     }
 }
---
+++ src/com/almasb/maze/MazeGenerator.java
@@ -0,0 +1,91 @@
+package com.almasb.maze;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Modified and adapted for general use version of
+ * recursive backtracking algorithm
+ * shamelessly borrowed from the ruby at
+ * http://weblog.jamisbuck.org/2010/12/27/maze-generation-recursive-backtracking
+ *
+ * @author Almas Baimagambetov (ab607@uni.brighton.ac.uk)
+ * @version 1.0
+ */
+public class MazeGenerator {
+    private final int x;
+    private final int y;
+    private final int[][] maze;
+
+    private MazeCell[][] theMaze;
+
+    public MazeGenerator(int x, int y) {
+        this.x = x;
+        this.y = y;
+        maze = new int[this.x][this.y];
+        generateMaze(0, 0);
+        adapt();
+    }
+
+    public MazeCell[][] getMaze() {
+        return theMaze;
+    }
+
+    private void adapt() {
+        theMaze = new MazeCell[x][y];
+
+        for (int i = 0; i < y; ++i) {
+            for (int j = 0; j < x; ++j) {
+                MazeCell cell = new MazeCell();
+                if ((maze[j][i] & 1) == 0) cell.topWall = true;
+                if ((maze[j][i] & 8) == 0) cell.leftWall = true;
+
+                theMaze[j][i] = cell;
+            }
+        }
+    }
+
+    private void generateMaze(int cx, int cy) {
+        DIR[] dirs = DIR.values();
+        Collections.shuffle(Arrays.asList(dirs));
+        for (DIR dir : dirs) {
+            int nx = cx + dir.dx;
+            int ny = cy + dir.dy;
+            if (between(nx, x) && between(ny, y) && (maze[nx][ny] == 0)) {
+                maze[cx][cy] |= dir.bit;
+                maze[nx][ny] |= dir.opposite.bit;
+                generateMaze(nx, ny);
+            }
+        }
+    }
+
+    private static boolean between(int v, int upper) {
+        return (v >= 0) && (v < upper);
+    }
+
+    private enum DIR {
+        N(1, 0, -1), S(2, 0, 1), E(4, 1, 0), W(8, -1, 0);
+        private final int bit;
+        private final int dx;
+        private final int dy;
+        private DIR opposite;
+
+        // use the static initializer to resolve forward references
+        static {
+            N.opposite = S;
+            S.opposite = N;
+            E.opposite = W;
+            W.opposite = E;
+        }
+
+        private DIR(int bit, int dx, int dy) {
+            this.bit = bit;
+            this.dx = dx;
+            this.dy = dy;
+        }
+    }
+
+    public static class MazeCell {
+        public boolean topWall = false, leftWall = false;
+    }
+}
